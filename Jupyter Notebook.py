59e8b862a6d769bedf9a8adb86f0c2de7a167c65
duarteke <125162397+duarteke@users.noreply.github.com>
2023-02-22 15:18:40 -0700

    Add files via upload

---
+++ Jupyter Notebook.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Jorge Duarte Keinrad**
+# <br>My current occupation is Telecommunications Engineer 
+
+# *I am interested in data science because it is getting more relevant by the hour and with the amount of data being collected by all devices and services I can see this becoming the next big carrier in the industry*
+
+# ### The code is supposed to get the average  out of three different numbers 
+
+# In[ ]:
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+
+# In[ ]:
+
+
+average = (num1 + num2 + num3) / 3
+
+
+# In[ ]:
+
+
+print("The average of the three numbers is:", average)
+
+
+# - This is how you get an average 
+# <br> 1. This is how you get an average 
+# *** 
+# 
